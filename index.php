1cd0eca5ec555146cde8faffc73c4e7f29439fdf
paulizzda12 <81577208+paulizzda12@users.noreply.github.com>
2021-12-23 16:41:10 +0100

    Added navbar

--- index.php
+++ index.php
@@ -9,7 +9,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <div class="justify-content-start">
+                <a class="navbar-brand" href="#">
+                    <img src="files/images/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+                    BEWÄSY
+                </a>
+            </div>
 
+            <form class="justify-content-end">
+                        <button class="btn btn-outline-secondary me-2" type="button" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
+                        <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#registrationModal">Registrieren</button>
+            </form>
+        </div>
+    </nav>
+
+
+
+    <h1>LANDING PAGE</h1>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
