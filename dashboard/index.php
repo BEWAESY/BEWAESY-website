4774be04ad6ff09a91dc16a4c483a9ef369d4dd6
Johannes <johannes.kuerz@gmx.de>
2021-12-27 12:40:15 +0100

    Added Dashboard page

---
+++ dashboard/index.php
@@ -0,0 +1,30 @@
+<?php include "../files/php/config/config.php" ?>
+
+<?php
+    session_start();
+
+    // Check if user is logged in, if not, redirect to login
+    if (!isset($_SESSION["userid"])) {
+        header("Location: ".$filePath."login?redirect=dashboard");
+        die("Bitte zuerst <a href='".$filePath."login'>einloggen</a>");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BEWÄSY</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <?php include "../files/php/templates/nav.php" ?>
+
+    <h1>DASHBOARD</h1>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
