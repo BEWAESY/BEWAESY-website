11e73180ca7ae513cdc062f108dc0d8820725486
Johannes <johannes.kuerz@gmx.de>
2022-01-13 18:49:53 +0100

    Tried working on chart

--- files/js/statistics.js
+++ files/js/statistics.js
@@ -1,7 +1,58 @@
 // Graphs
 var ctx = document.getElementById('myChart')
 // eslint-disable-next-line no-unused-vars
+
+
+var data = {
+    labels: [
+        "-14",
+        "-13",
+        "-12",
+        "-11",
+        "-10",
+        "-9",
+        "-8",
+        "-7",
+        "-6",
+        "-5",
+        "-4",
+        "-3",
+        "-2",
+        "gestern",
+        "heute"
+    ],
+    datasets: [
+        {
+            label: "System 1",
+            data: [
+                "5",
+                "10",
+            ],
+            //borderColor: Utils.CHART_COLORS.red,
+            backgroundColor: "transperent",
+        }
+    ]
+};
+
 var myChart = new Chart(ctx, {
+    type: "line",
+    data: data,
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "top",
+            },
+            title: {
+                display: true,
+                text: "Gießzeiten von allen Bewässerungssystemen in den letzten 14 Tagen"
+            }
+        }
+    },
+});
+
+
+/*var myChart = new Chart(ctx, {
     type: 'line',
     data: {
     labels: [
@@ -15,16 +66,7 @@ var myChart = new Chart(ctx, {
         "asdf"
     ],
     datasets: [{
-        data: [
-        0,
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7
-        ],
+        data: data,
         lineTension: 0,
         backgroundColor: 'transparent',
         borderColor: '#007bff',
@@ -44,4 +86,4 @@ var myChart = new Chart(ctx, {
         display: false
     }
     }
-})
+})*/
