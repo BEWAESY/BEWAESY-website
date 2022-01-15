d4af0d0f95de804b73f55045060aade7d9996554
Johannes <johannes.kuerz@gmx.de>
2022-01-15 18:55:06 +0100

    Created Impressum + Datenschutz

---
+++ impressum.php
@@ -0,0 +1,32 @@
+<?php include "files/php/config/config.php" ?>
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Impressum - BEWÄSY</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <?php include "files/php/templates/nav.php" ?>
+
+    <div style="margin: 30px">
+        <h1>Impressum</h1>
+
+        <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+        <p>Johannes Kürz<br />
+        Wehrleshalde 25<br />
+        73434 Aalen</p>
+
+        <h2>Kontakt</h2>
+        <p>Telefon: +49 (0) 157 73 15 61 35<br />
+        E-Mail: johannes.kuerz@gmx.de</p>
+
+        <p>Quelle: <a href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a></p>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -9,12 +9,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BEWÄSY</title>
 
+    <link href="files/css/main.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <?php include "files/php/templates/nav.php" ?>
 
     <h1>LANDING PAGE</h1>
+
+    <div class="footer-container">
+        <footer class="my-4">
+            <ul class="nav justify-content-center">
+                <li class="nav-item"><a href="." class="nav-link px-2 text-muted">Home</a></li>
+                <li class="nav-item"><a href="impressum.php" class="nav-link px-2 text-muted">Impressum</a></li>
+                <li class="nav-item"><a href="datenschutz.php" class="nav-link px-2 text-muted">Datenschutzerklärung</a></li>
+            </ul>
+        </footer>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
