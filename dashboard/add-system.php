d905e5cc3cdff9be5932ae7308353b3aa3170200
Johannes <johannes.kuerz@gmx.de>
2022-01-19 21:20:57 +0100

    Added add-system page + Improvements in checking authentication on each page

---
+++ dashboard/add-system.php
@@ -0,0 +1,45 @@
+<?php
+    session_start();
+    include "../files/php/config/config.php";
+
+    $page = "add-system";
+
+    // Check authentication
+    include "../files/php/templates/check-authentication.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>System hinzufügen - BEWÄSY</title>
+
+    <link rel="icon" type="image/x-icon" href="../files/images/logo.svg">
+
+    <link href="../files/addons/bootstrap.min.css" rel="stylesheet">
+    <link href="../files/css/dashboard.css" rel="stylesheet">
+</head>
+<body>
+    <?php include "../files/php/templates/nav.php" ?>
+
+
+    <div class="dashboard-container">
+        <?php include "../files/php/templates/dashboard-nav.php"; ?>
+
+        <div class="main">
+            <div class="card" style="margin: 20px">
+                <div class="card-body">
+                    <h1>Ein neues Bewässerungssystem hinzufügen</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="../files/addons/jquery-3.6.0.min.js"></script>
+    <script src="../files/addons/bootstrap.bundle.min.js"></script>
+</body>
+</html>
