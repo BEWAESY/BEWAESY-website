5977db8f0e715f7c8474d2c455e766707773f1f3
Johannes <johannes.kuerz@gmx.de>
2022-02-13 17:23:35 +0100

    Added a slider to the landing page

--- index.php
+++ index.php
@@ -16,9 +16,34 @@
 <body>
     <?php include "files/php/templates/nav.php" ?>
 
-    <h1>LANDING PAGE</h1>
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner" style="height: 500px;">
+            <div class="carousel-item active">
+                <img src="files/images/slider/Blumentopf1.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="files/images/slider/Technikum.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="files/images/slider/3D-Drucker.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 
-    <?php include "files/php/templates/footer.php" ?>
+    <?php //include "files/php/templates/footer.php" ?>
 
     <script src="files/addons/bootstrap.bundle.min.js"></script>
 </body>
