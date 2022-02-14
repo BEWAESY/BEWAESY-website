f7774f5c5c4aa4333838df8900104698cef1d1c3
Johannes <johannes.kuerz@gmx.de>
2022-02-14 21:30:57 +0100

    Design improvements

--- files/php/templates/nav.php
+++ files/php/templates/nav.php
@@ -1,5 +1,5 @@
 <!--<nav class="navbar navbar-light bg-light" style="height: 54px; box-shadow: 1px 0 10px rgb(156, 156, 156);">-->
-<nav class="navbar navbar-light bg-light" style="height: 54px">
+<nav class="navbar navbar-light" style="height: 54px; background-color: #f2f2f2;">
     <div class="container-fluid">
         <div class="justify-content-start">
             <a class="navbar-brand" href="<?php echo($filePath); ?>">
